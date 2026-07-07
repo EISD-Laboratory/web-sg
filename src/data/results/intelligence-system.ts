f2191c8b881ac414984118f5060d5cf78e846bac
Hilmi Zikri <zikrihilmi15@gmail.com>
2026-07-07 09:52:06 +0700

    update data

--- src/data/results/intelligence-system.ts
+++ src/data/results/intelligence-system.ts
@@ -1,52 +1,10 @@
 import { StudentResult } from "./types";
 
 export const IS_RESULTS: StudentResult[] = [
-  {
+  /*{
     name: "Nama Contoh",
     nim: "0000000000000",
     division: "Intelligence System",
     status: "Passed",
-  },
-  {
-    name: "Nama Contoh",
-    nim: "0000000000000",
-    division: "Intelligence System",
-    status: "Passed",
-  },
-  {
-    name: "Nama Contoh",
-    nim: "0000000000000",
-    division: "Intelligence System",
-    status: "Passed",
-  },
-  {
-    name: "Nama Contoh",
-    nim: "0000000000000",
-    division: "Intelligence System",
-    status: "Passed",
-  },
-  {
-    name: "Nama Contoh",
-    nim: "0000000000000",
-    division: "Intelligence System",
-    status: "Passed",
-  },
-  {
-    name: "Nama Contoh",
-    nim: "0000000000000",
-    division: "Intelligence System",
-    status: "Passed",
-  },
-  {
-    name: "Nama Contoh",
-    nim: "0000000000000",
-    division: "Intelligence System",
-    status: "Passed",
-  },
-  {
-    name: "Nama Contoh",
-    nim: "0000000000000",
-    division: "Intelligence System",
-    status: "Failed",
-  },
+  },*/
 ];
--- src/data/results/uiux.ts
+++ src/data/results/uiux.ts
@@ -1,76 +1,10 @@
 import { StudentResult } from "./types";
 
 export const UIUX_RESULTS: StudentResult[] = [
-  {
+  /*{
     name: "Nama Contoh",
     nim: "0000000000000",
     division: "UI/UX Design",
     status: "Passed",
-  },
-  {
-    name: "Nama Contoh",
-    nim: "0000000000000",
-    division: "UI/UX Design",
-    status: "Passed",
-  },
-  {
-    name: "Nama Contoh",
-    nim: "0000000000000",
-    division: "UI/UX Design",
-    status: "Passed",
-  },
-  {
-    name: "Nama Contoh",
-    nim: "0000000000000",
-    division: "UI/UX Design",
-    status: "Passed",
-  },
-  {
-    name: "Nama Contoh",
-    nim: "0000000000000",
-    division: "UI/UX Design",
-    status: "Passed",
-  },
-  {
-    name: "Nama Contoh",
-    nim: "0000000000000",
-    division: "UI/UX Design",
-    status: "Passed",
-  },
-  {
-    name: "Nama Contoh",
-    nim: "0000000000000",
-    division: "UI/UX Design",
-    status: "Passed",
-  },
-  {
-    name: "Nama Contoh",
-    nim: "0000000000000",
-    division: "UI/UX Design",
-    status: "Passed",
-  },
-  {
-    name: "Nama Contoh",
-    nim: "0000000000000",
-    division: "UI/UX Design",
-    status: "Passed",
-  },
-  {
-    name: "Nama Contoh",
-    nim: "0000000000000",
-    division: "UI/UX Design",
-    status: "Passed",
-  },
-  {
-    name: "Nama Contoh",
-    nim: "0000000000000",
-    division: "UI/UX Design",
-    status: "Passed",
-  },
-  {
-    name: "Nama Contoh",
-    nim: "0000000000000",
-    division: "UI/UX Design",
-    status: "Passed",
-  },
+  },*/
 ];
